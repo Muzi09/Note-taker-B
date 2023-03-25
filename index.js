c5d87d3811e21f40274be9d6653b0c623ae87715
Muzi09 <amzikhan711@gmail.com>
2023-03-25 22:59:56 +0530

    MONGO_URI Added

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ app.use(bodyParser.json())
 app.use(cors())
 
 try {
-    mongoose.connect('mongodb://127.0.0.1:27017/Note_Taker')
+    mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
     console.log('Connected to MongoDB')
 }
-catch (err) {
+catch (err) {   
     console.log(err)
 }
 
